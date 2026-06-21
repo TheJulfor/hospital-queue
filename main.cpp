9c658e21657fc0af3b12369271bb0e9dbc2046e4
TheJulfor <romagordzilevsky@gmail.com>
2026-06-21 15:47:19 +0500

    Implement command execution + fix open file check

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include "hospital.h"
 
 int main() {
     system("chcp 65001 > nul");
@@ -11,12 +12,33 @@ int main() {
     std::cin >> filename;
 
     std::ifstream ifile(filename);
-    if (ifile.is_open()) {
+    if (!ifile.is_open()) {
         std::cout << "!! Ошибка: не удалось открыть файл.";
         return 1;
     }
 
-    // handle commands
+    int count;
+    if (!(ifile >> count)) {
+        return 0;
+    }
+
+    Hospital hospital;
+    std::cout << "Обслуженные пациенты:\n";
+
+    for (int i = 0; i < count; i++) {
+        std::string command;
+        ifile >> command;
+
+        if (command == "+") {
+            std::string name;
+            int priority;
+            ifile >> name >> priority;
+            hospital.addPatient(name, priority);
+        }
+        else if (command == "-") {
+            hospital.servePatient();
+        }
+    }
 
     ifile.close();
     return 0;
